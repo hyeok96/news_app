fe514073ab41700775be73f8535c9f38843bd561
hyeok96 <saing0813@naver.com>
2023-04-19 18:54:16 +0900

    news_app publishing and animation

---
+++ lib/newsDetail_screen.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+class NewsDetailScreen extends StatefulWidget {
+  const NewsDetailScreen({
+    super.key,
+    this.data,
+  });
+
+  final Map<dynamic, dynamic>? data;
+
+  @override
+  State<NewsDetailScreen> createState() => _NewsDetailScreenState();
+}
+
+class _NewsDetailScreenState extends State<NewsDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Hero(
+                tag: widget.data!["id"],
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: Image.network(
+                    widget.data!["thumbnail"],
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 16,
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      widget.data!["title"],
+                      style: const TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "by ${widget.data!["author"]}",
+                          style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          "Full Article",
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.blue.shade800,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const Divider(
+                      height: 60,
+                    ),
+                    Text(
+                      widget.data!["content"],
+                      style: const TextStyle(
+                        fontSize: 18,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/newsHome_screen.dart
@@ -0,0 +1,135 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:news_app/newsDetail_screen.dart';
+
+class NewsHomeScreen extends StatefulWidget {
+  const NewsHomeScreen({super.key});
+
+  @override
+  State<NewsHomeScreen> createState() => _NewsHomeScreenState();
+}
+
+class _NewsHomeScreenState extends State<NewsHomeScreen> {
+  var dio = Dio();
+
+  Future? _newsList() async {
+    var res =
+        await dio.get("http://52.79.115.43:8090/api/collections/news/records");
+    if (res.data != null) {
+      return res.data!["items"];
+    } else {
+      return null;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("News"),
+        centerTitle: false,
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+      ),
+      body: FutureBuilder(
+        future: _newsList(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.separated(
+              itemBuilder: (context, index) {
+                var firstContent = snapshot.data![index]["content"].toString();
+                return Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 16,
+                  ),
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) =>
+                              NewsDetailScreen(data: snapshot.data[index]),
+                        ),
+                      );
+                    },
+                    child: Stack(
+                      children: [
+                        Hero(
+                          tag: snapshot.data![index]["id"],
+                          child: ZoomIn(
+                            child: AspectRatio(
+                              aspectRatio: 16 / 9,
+                              child: Container(
+                                padding: const EdgeInsets.symmetric(
+                                  horizontal: 16,
+                                  vertical: 8,
+                                ),
+                                decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      image: NetworkImage(
+                                        snapshot.data![index]["thumbnail"],
+                                      ),
+                                      fit: BoxFit.cover,
+                                      colorFilter: const ColorFilter.mode(
+                                        Colors.black45,
+                                        BlendMode.darken,
+                                      )),
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Positioned.fill(
+                          bottom: 0,
+                          child: ZoomIn(
+                            duration: const Duration(
+                              milliseconds: 300,
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 16,
+                                vertical: 8,
+                              ),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    snapshot.data![index]["title"],
+                                    style: const TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(
+                                    firstContent.substring(
+                                      0,
+                                      firstContent.indexOf("."),
+                                    ),
+                                    style: const TextStyle(fontSize: 14),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+              separatorBuilder: (context, index) {
+                return const SizedBox(
+                  height: 16,
+                );
+              },
+              itemCount: snapshot.data!.length,
+            );
+          }
+          return const CircularProgressIndicator();
+        },
+      ),
+    );
+  }
+}
